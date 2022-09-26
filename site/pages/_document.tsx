c2a0df731fe26fb05fc2f02e3f0eca233088d49a
Everyone <91630063+letzwrk@users.noreply.github.com>
2022-09-26 15:04:48 -0500

    added global tag <script/>

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -4,7 +4,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          async src=
+          {`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          dangerouslySetInnerHTML=
+          {{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />
